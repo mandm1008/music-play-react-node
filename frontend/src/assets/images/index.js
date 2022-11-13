e94e6097c378ea7cb4deffa2dc21fbaba0e63401
mandm1008 <dominhmanupwork1306@gmail.com>
2022-11-13 20:40:41 +0700

    init data

---
+++ frontend/src/assets/images/index.js
@@ -0,0 +1,12 @@
+const images = {
+  logo: require('./svgs/logo.svg').default,
+  music: require('./svgs/music.svg').default,
+  playlist: require('./svgs/playlist.svg').default,
+  history: require('./svgs/history.svg').default,
+  live: require('./svgs/live.svg').default,
+  theme: require('./svgs/theme.svg').default,
+  album: require('./svgs/album.svg').default,
+  uploadMusic: require('./svgs/uploadMusic.svg').default
+}
+
+export default images
