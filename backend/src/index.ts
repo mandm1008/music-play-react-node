e1cf24b7eb31eef10c40982ad07ebbdb50fc7768
mandm1008 <dominhmanupwork1306@gmail.com>
2022-11-14 23:38:01 +0700

    add cors origin

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
+import cors from 'cors'
 import routing from './routers'
 
 const app = express()
 const port = 3030
 
 app.use(express.json())
+app.use(cors())
 
 routing(app)
 
